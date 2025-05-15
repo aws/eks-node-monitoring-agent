d39b522b90609c55db38c3f3d74cdc046e9839eb
Nick Baker <nbakerd@amazon.com>
2025-05-14 17:57:44 -0700

    fix(ci): update publish script (#8)

--- hack/publish-charts.sh
+++ hack/publish-charts.sh
@@ -7,7 +7,24 @@ VERSION=$(git describe --tags --always)
 STAGING_DIR=${1:-${GIT_REPO_ROOT}}
 CHART_URL=${2:-https://aws.github.io/eks-node-monitoring-agent}
 
+git fetch --all
+git config user.email eks-bot@users.noreply.github.com
+git config user.name eks-bot
+
 helm package ${GIT_REPO_ROOT}/charts/* --destination ${STAGING_DIR} --dependency-update
+
+for bundle in ${STAGING_DIR}/*.tgz; do
+    if git cat-file -e origin/gh-pages:${bundle}; then
+        echo "⏩ Release already exists for ${bundle}"
+        rm -f ${bundle}
+    fi
+done
+
+if ! ls ${STAGING_DIR}/*.tgz; then
+    echo "⏩ No changes to be staged"
+    exit 0
+fi
+
 git checkout gh-pages
 mv ${STAGING_DIR}/*.tgz .
 helm repo index . --url ${CHART_URL}
